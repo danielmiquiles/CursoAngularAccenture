d6db0c66cf24d0d15410f125eebac1d4ea8685fa
Daniel Miquiles <danielvictormiquiles@gmail.com>
2021-02-17 12:01:18 -0300

    instalando bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -6,7 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
   },
   {
@@ -16,6 +17,15 @@ const routes: Routes = [
   {
     path: 'extrato',
     component: ExtratoComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NaoEncontradoComponent
   },
 ];
 
