03db22c31a215965d639ec3103d04d30e6bf27f4
Daniel Miquiles <danielvictormiquiles@gmail.com>
2021-02-16 21:50:09 -0300

    i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ContadorComponent } from './contador/contador.component';
 import { DiretivasComponent } from './diretivas/diretivas.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PipeComponent } from './pipe/pipe.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -16,13 +21,17 @@ import { DiretivasComponent } from './diretivas/diretivas.component';
     HomeComponent,
     FooterComponent,
     ContadorComponent,
-    DiretivasComponent
+    DiretivasComponent,
+    PipeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
