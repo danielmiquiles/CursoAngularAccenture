947b8f81e37d0a856c8d5204ebd7bcfaa19f93ec
Daniel Miquiles <danielvictormiquiles@gmail.com>
2021-02-19 12:38:11 -0300

    introdução a formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,8 @@ import { LoginComponent } from './login/login.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { SharedModule } from './shared/shared.module';
 import { ContatosComponent } from './contatos/contatos.component';
+import { FormsModule } from '@angular/forms';
 
-// import { ContadorComponent } from './shared/components/contador/contador.component';
-// import { DiretivasComponent } from './shared/components/diretivas/diretivas.component';
-
-// import { ExercicioNgclassComponent } from './shared/components/exercicio-ngclass/exercicio-ngclass.component';
-
-// import { FooterComponent } from './shared/components/footer/footer.component';
-// import { HeaderComponent } from './shared/components/header/header.component';
-// import { PipeComponent } from './shared/components/pipe/pipe.component';
-// import { ExercicioPipeComponent } from './shared/components/exercicio-pipe/exercicio-pipe.component';
 registerLocaleData(localePt);
 
 
@@ -32,18 +24,12 @@ registerLocaleData(localePt);
     ExtratoComponent,
     NaoEncontradoComponent,
     ContatosComponent,
-    // HeaderComponent,
-    // FooterComponent,
-    // ContadorComponent,
-    // DiretivasComponent,
-    // ExercicioNgclassComponent,
-    // PipeComponent,
-    // ExercicioPipeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    FormsModule,
+    SharedModule,
   ],
   providers: [{
     provide: LOCALE_ID,
--- src/app/contatos/contatos.service.ts
+++ src/app/contatos/contatos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from './../../environments/environment';
@@ -11,9 +11,29 @@ export class ContatosService {
 
   API_URL = environment.API_URL
 
+  //PASSA ISSO COM 3° PARAMETRO NO CREATE POR EXEMPLO
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: 'TOKEN AQUI'
+    })
+  }
+
   constructor(private http: HttpClient) { }
 
   getContatos(){
     return this.http.get<Contatos[]>(`${this.API_URL}/contatos`)
+  }
+
+
+  createContato(contato: Contatos){
+    return this.http.post<Contatos[]>(`${this.API_URL}/contatos`, contato,)
+  }
+
+  updateContato(id: string, contato: Contatos){
+    return this.http.put<Contatos[]>(`${this.API_URL}/contatos/${id}`, contato)
+  }
+
+  deleteContato(id: string){
+    return this.http.delete<Contatos[]>(`${this.API_URL}/contatos/${id}`)
   }
 }
