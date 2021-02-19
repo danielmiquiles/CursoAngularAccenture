a21b56fa9e3719a9a5e8dee61bbee8f2630e5916
Daniel Miquiles <danielvictormiquiles@gmail.com>
2021-02-19 15:34:17 -0300

    template driven formulário

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  @ViewChild('emailInput') emailInput: ElementRef
+  @ViewChild('passwordInput') passwordInput: ElementRef
 
-  ngOnInit(): void {
-  }
+  email: string;
+  password: string;
 
   onSubmit(form){
-    console.log(form);
+    if(!form.valid){
+      form.controls.email.markAsTouched();
+      form.controls.password.markAsTouched();
+
+      if(form.controls.email.invalid){
+        this.emailInput.nativeElement.focus();
+        return;
+      } else {
+        this.passwordInput.nativeElement.focus();
+        return;
+      }
+    }
   }
 
+  exibeErro(nomeControle: string, form: FormGroup){
+    if(!form.controls[nomeControle]){
+      return false;
+    }
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
+  }
 }
